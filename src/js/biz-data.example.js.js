f672463704ae77cb4976733aad9d43b7a35b0eaa
htran174 <hien.vu.tran2002@gmail.com>
2025-11-22 21:41:49 -0800

    synic

--- src/js/biz-data.example.js.js
+++ src/js/biz-data.example.js.js
@@ -1,7 +1,18 @@
+// src/js/biz-data.js
 export const BIZ = {
-  name: "Your Salon Name",
-  phone: "",
-  address: "",
-  hours: [],
-  socials: {}
+  name: "Store Name",
+  phone: "(999) 999-999",
+  phone_link: "+17017612828",
+  address: "2800 1st Street S, Mary, Ca 92222",
+  map_link: "https://www.google.com",
+  hours: [
+    { label: "Mon–Fri", value: "10:00 AM – 7:00 PM" },
+    { label: "Sat",    value: "10:00 AM – 7:00 PM" },
+    { label: "Sun",    value: "12:00 PM – 5:00 PM" },
+  ],
+  socials: {
+    //Instagram: "https://instagram.com/",
+    Facebook:  "https://www.facebook.com",
+    //TikTok:    "https://tiktok.com/"
+  }
 };
